5d7bdcd2729b74ace03246ca206d46dce1f18f78
Shahan Iresh <shahaniresh7@gmail.com>
2025-12-19 08:47:31 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,26 @@
 'use strict';
- 
+
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
- 
+
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
- 
+
 sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
- s
+
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
- 
+
 const modalImg = document.querySelector("[data-modal-img]");
 const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
- 
+
 const testimonialsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
- 
+
 for (let i = 0; i < testimonialsItem.length; i++) {
 
   testimonialsItem[i].addEventListener("click", function () {
@@ -35,17 +35,17 @@ for (let i = 0; i < testimonialsItem.length; i++) {
   });
 
 }
- 
+
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
- 
+
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
 select.addEventListener("click", function () { elementToggleFunc(this); });
- 
+
 for (let i = 0; i < selectItems.length; i++) {
   selectItems[i].addEventListener("click", function () {
 
@@ -74,7 +74,7 @@ const filterFunc = function (selectedValue) {
   }
 
 }
- 
+
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
@@ -92,14 +92,14 @@ for (let i = 0; i < filterBtn.length; i++) {
   });
 
 }
- 
+
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
 const formBtn = document.querySelector("[data-form-btn]");
- 
+
 for (let i = 0; i < formInputs.length; i++) {
   formInputs[i].addEventListener("input", function () {
- 
+
     if (form.checkValidity()) {
       formBtn.removeAttribute("disabled");
     } else {
@@ -108,10 +108,10 @@ for (let i = 0; i < formInputs.length; i++) {
 
   });
 }
- 
+
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
- 
+
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
